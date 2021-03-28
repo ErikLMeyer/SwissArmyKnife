08898b253164076678611d802eb885f4f338895e
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-28 14:56:13 -0500

    Wrote body
    Prints out information about the available commands.

--- Help.java
+++ Help.java
@@ -1,5 +1,9 @@
 public class Help {
-    public static void printH(){
-        System.out.println("Help under construction.");
-    }    
+    public static void printH() {
+        System.out.println("-Help : Prints this information");
+        System.out.print("-HttpRequest [URL] : Prints out a text representation of the given");
+        System.out.print(" URL.\n");
+        System.out.print("-HttpRequestIndex [URL] : Prints out a text representation of the ");
+        System.out.print("given URL, as well as any other URLs present on the site.\n");
+    }
 }
