8409f7f561f0eb663658282a2ca1f50793ae1a34
ErikLMeyer <erikmeyer@lewisu.edu>
2021-04-11 23:13:14 -0500

    Added commands
    SleepFast and SleepFastRunnable

--- sak.java
+++ sak.java
@@ -36,6 +36,35 @@ public class sak {
         } else if (args[0].equalsIgnoreCase("-sleep")) {
             Sleep mySleeper = new Sleep(0);
             mySleeper.sleep();
+        } else if (args[0].equalsIgnoreCase("-sleepfast")){
+            SleepFast mySleeper0 = new SleepFast(0);
+            SleepFast mySleeper1 = new SleepFast(1);
+
+            mySleeper0.start();
+            mySleeper1.start();
+
+            try{
+                mySleeper0.join();
+                mySleeper1.join();
+            } catch(Exception e){
+                System.out.println("Something broke. Exception = " + e);
+            }
+        } else if (args[0].equalsIgnoreCase("-sleepfastrunnable")){
+            SleepFastRunnable mySleeper0 = new SleepFastRunnable(0);
+            SleepFastRunnable mySleeper1 = new SleepFastRunnable(1);
+
+            Thread myThread0 = new Thread(mySleeper0);
+            Thread myThread1 = new Thread(mySleeper1);
+
+            myThread0.start();
+            myThread1.start();
+
+            try{
+                myThread0.join();
+                myThread1.join();
+            } catch(Exception e){
+                System.out.println("Something broke. Exception = " + e);
+            }
         }
 
         System.out.println("Application took " + (System.currentTimeMillis() - start) + "ms to complete.");
