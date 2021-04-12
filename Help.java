182a8b79d8f521fbf9e5cc895b03430209c4b9f3
ErikLMeyer <erikmeyer@lewisu.edu>
2021-04-11 23:16:50 -0500

    Updated Help

--- Help.java
+++ Help.java
@@ -5,7 +5,7 @@ public class Help {
         System.out.print(" URL.\n");
         System.out.print("-HttpRequestIndex [URL] : Prints out a text representation of the ");
         System.out.print("given URL, as well as any other URLs present on the site.\n");
-        System.out.println("\nCommands that are coming soon:");
-        System.out.println("-Sleep\n-SleepFast\n-SleepFastImplementsRunnable\n-HttpValidateIndex");
+        System.out.println("These sleep.\n-Sleep\n-SleepFast\n-SleepFastImplementsRunnable");
+        System.out.println("\nCommands that are coming soon:\n-HttpValidateIndex");
     }
 }
